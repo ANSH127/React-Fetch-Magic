897d0207b10fbd73e36c09e2c84f596a743bde1b
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-11 15:50:28 +0530

    making initialData Quary

--- src/hooks/useSuperHeroDetail.js
+++ src/hooks/useSuperHeroDetail.js
@@ -1,10 +1,22 @@
 
-import { useQuery } from '@tanstack/react-query'
+import { useQuery,useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 
-export const useSuperHeroDetail = (id) =>useQuery({
+export const useSuperHeroDetail = (id) =>{
+    const queryClient = useQueryClient()
+
+    return useQuery({
+    
     queryKey: ['superherodetail',id],
     queryFn: () =>
         axios.get(`http://localhost:4000/superheroes/${id}`).then((res) => res.data),
+    initialData: () => {
+
+        const superheros = queryClient.getQueryData(['superherodata'])
+        return superheros?.find((superhero) => superhero.id ===parseInt(id))
+    }
+
+
 
 })
+}
