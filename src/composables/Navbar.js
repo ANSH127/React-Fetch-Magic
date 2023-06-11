4cf652fed3dd03200fbc5e1d6e33580e61e5b05c
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-11 10:31:14 +0530

    updating file

--- src/composables/Navbar.js
+++ src/composables/Navbar.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">Navbar</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Navbar</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/super-heros">Super-heros</Link>
+        <li className="nav-item">
+          <Link className="nav-link" to="/super-heros">Super-heros</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/rq-super-heros">RQSuper-heros</Link>
+        <li className="nav-item">
+          <Link className="nav-link" to="/rq-super-heros">RQSuper-heros</Link>
         </li>
       </ul>
     </div>
