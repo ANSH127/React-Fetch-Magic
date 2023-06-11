1ba69921d0d79e8734fa1963d6dc0ecbb0aac9f1
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-11 10:44:09 +0530

    custom quary hook

--- src/components/RQSuperHerosPage.js
+++ src/components/RQSuperHerosPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+// import { useQuery } from '@tanstack/react-query'
+// import axios from 'axios'
+import {useSuperHerosData} from '../hooks/useSuperHerosData'
 
 export default function RQSuperHerosPage() {
 
@@ -10,20 +11,7 @@ export default function RQSuperHerosPage() {
   const onError=(error)=>{
     console.log('perform some action when the data is not fetched successfully',error)
   }
-  const { isLoading, error, data,refetch } = useQuery({
-    queryKey: ['superherodata'],
-    queryFn: () =>
-      axios.get('http://localhost:4000/superheroes').then((res) => res.data),
-    // staleTime: 20000, //it means it will pause the request for 20 seconds
-    // refetchOnMount:true, //it means it will refetch the data when the component mounts
-    // refetchOnWindowFocus:true, //it means it will refetch the data when the window is in focus
-    // refetchInterval:2000, //it means it will refetch the data every 2 seconds
-    // refetchIntervalInBackground:true, //it means it will refetch the data every 2 seconds even if the window is not in focus
-    enabled:false, //it means it will not fetch the data when the component mounts
-    onSuccess:onSuccess,
-    onError:onError
-    
-  })
+  const { isLoading, error, data,refetch } = useSuperHerosData(onSuccess,onError)
 
   // if (isLoading) return 'Loading...'
 
