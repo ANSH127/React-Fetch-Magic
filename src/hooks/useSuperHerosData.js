f0f3fb24ed800909e98eb1d01c382d3d367e981c
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-11 23:21:01 +0530

    using setQuaryData

--- src/hooks/useSuperHerosData.js
+++ src/hooks/useSuperHerosData.js
@@ -1,31 +1,35 @@
 
-import { useQuery,useMutation,useQueryClient } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 
-export const useSuperHerosData = (onSuccess,onError) =>useQuery({
-    queryKey: ['superherodata'],
-    queryFn: () =>
-      axios.get('http://localhost:4000/superheroes').then((res) => res.data),
-    // staleTime: 20000, //it means it will pause the request for 20 seconds
-    // refetchOnMount:true, //it means it will refetch the data when the component mounts
-    // refetchOnWindowFocus:true, //it means it will refetch the data when the window is in focus
-    // refetchInterval:2000, //it means it will refetch the data every 2 seconds
-    // refetchIntervalInBackground:true, //it means it will refetch the data every 2 seconds even if the window is not in focus
-    enabled:false, //it means it will not fetch the data when the component mounts
-    onSuccess:onSuccess, //it means it will perform some action when the data is fetched successfully
-    onError:onError, //it means it will perform some action when the data is not fetched successfully
-    
-    
-  })
+export const useSuperHerosData = (onSuccess, onError) => useQuery({
+  queryKey: ['superherodata'],
+  queryFn: () =>
+    axios.get('http://localhost:4000/superheroes').then((res) => res.data),
+  // staleTime: 20000, //it means it will pause the request for 20 seconds
+  // refetchOnMount:true, //it means it will refetch the data when the component mounts
+  // refetchOnWindowFocus:true, //it means it will refetch the data when the window is in focus
+  // refetchInterval:2000, //it means it will refetch the data every 2 seconds
+  // refetchIntervalInBackground:true, //it means it will refetch the data every 2 seconds even if the window is not in focus
+  enabled: false, //it means it will not fetch the data when the component mounts
+  onSuccess: onSuccess, //it means it will perform some action when the data is fetched successfully
+  onError: onError, //it means it will perform some action when the data is not fetched successfully
 
-  export const useAddSuperHero = () =>{
-  const queryClient=useQueryClient()
-    
-   return useMutation({
+
+})
+
+export const useAddSuperHero = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
     mutationFn: (data) =>
-      axios.post('http://localhost:4000/superheroes',data),
-    onSuccess:()=>queryClient.invalidateQueries({queryKey:['superherodata']}),
-    onError:()=>console.log('data not added successfully'),
+      axios.post('http://localhost:4000/superheroes', data),
+    onSuccess: (data) => {
+      
+      queryClient.setQueryData(['superherodata'], (oldData) => [...oldData, data.data])
+
+    },
+    onError: () => console.log('data not added successfully'),
 
   })
 }
